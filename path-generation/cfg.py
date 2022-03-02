306aae242c54058d6e293f783cad7302e315c2f4
sominsong97 <sominsong97@gmail.com>
2022-03-02 16:07:27 +0900

    added process for getting usecase of API function

--- path-generation/cfg.py
+++ path-generation/cfg.py
@@ -16,7 +16,6 @@ import subprocess
 
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-
 from tool import Logging
 log = Logging.Logging("info")
 
@@ -82,11 +81,11 @@ def make_cfg(eList):
         except Exception as e:
             log.error(f"Compile Error - {EID}")
             continue
-
-        cmd = f'cp {EID}.c.012t.cfg {TEMP_OTUPUT_PATH}'
+        # copy cfg and original
+        cmd = f'cp {EID}.c.012t.cfg {EID}.c.004t.original {TEMP_OTUPUT_PATH}'
         mv_result = subprocess.check_output(cmd,shell=True).decode().strip("\n")
         if mv_result == None:
-            log.error(f"move file error - maybe there is no {EID}.c.012t.cfg")
+            log.error(f"move file error - maybe there is no {EID}.c.012t.cfg or {EID}.c.004t.original")
             continue
 
         rm_result = subprocess.check_output('rm *',shell=True).decode().strip("\n")
@@ -94,7 +93,7 @@ def make_cfg(eList):
             log.error(f"remove error - maybe there is no a.out")
             continue
 
-        log.info(f"made CFG file({EID}.c.012t.cfg) for {EID}.c")
+        log.info(f"made CFG file({EID}.c.012t.cfg, {EID}.c.004t.original) for {EID}.c")
     
     os.chdir(cwd)
     os.system('rm -r /tmp/cfg/')
