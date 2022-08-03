1cff99a26e52975e84c753670c6dbb06b116167d
sominsong97 <sominsong97@gmail.com>
2022-08-03 20:26:46 +0900

    edit configure file

--- configure.sh
+++ configure.sh
@@ -14,6 +14,7 @@ pip install beautifulsoup4
 # Install Docker Engine
 apt-get install -y ca-certificates curl gnupg lsb-release
 ## Add Docker's official GPC Key
+mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
