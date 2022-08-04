ba6457aa9cd4940d6a4f15bbcb79c42ebccade57
sominsong97 <sominsong97@gmail.com>
2022-08-04 18:01:34 +0900

    edit do done statement in setdup file

--- setup.sh
+++ setup.sh
@@ -28,7 +28,8 @@ echo 1 > /sys/kernel/debug/tracing/events/raw_syscalls/sys_exit/enable
 # done
 
 # Let's pull Sominsong97 registry images
-docker_images=("myqalc" "mypdf2ps" "mylowriter" "myzip" "mygcc" "mygzip" "myopenjdk" "myhttpd" "mysql_slave" "mysql_master" "myredis" "mytomcat" "mymariadb" "mymongo" "newnode"
-for img in ${docker_images[@]} do
+docker_images=("myqalc" "mypdf2ps" "mylowriter" "myzip" "mygcc" "mygzip" "myopenjdk" "myhttpd" "mysql_slave" "mysql_master" "myredis" "mytomcat" "mymariadb" "mymongo" "newnode")
+for img in ${docker_images[@]}
+do
     docker pull sominsong97/hyper-seccomp:${img}
 done
