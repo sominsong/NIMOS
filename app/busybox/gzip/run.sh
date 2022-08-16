165daf4a55a4cfd6d10071ef5ed988b1d84f7fdb
sominsong97 <sominsong97@gmail.com>
2022-08-16 17:25:09 +0900

    leave only data folder

--- app/busybox/gzip/run.sh
+++
@@ -1,7 +0,0 @@
-# zip
-service strace-docker restart
-docker run --rm --name gzip-container  -v "$PWD":/home/ -w /home/ mygzip sh -c "sleep 2; gzip *.txt"
-
-# unzip
-service strace-docker restart
-docker run --rm --name gzip-container  -v "$PWD":/home/ -w /home/ mygzip sh -c "sleep 2; gzip -d *.gz"
