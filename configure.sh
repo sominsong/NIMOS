e0bdd1fc4bad37b831f824167b7eaa61f58ae43d
Somin SONG <sominsong97@gmail.com>
2023-05-22 05:36:34 +0900

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -32,7 +32,7 @@ docker run hello-world
 curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 apt-get update
-apt-get install -y mongodb-org
+apt-get install -y mongodb-org mongodb-mongosh
 apt install -y mysql-common
 apt install -y apache2-utils redis-tools
 
